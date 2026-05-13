655900595dca3e51fa351b1b693ba393e02cf8b1
Giulio Garzia <32651700+Ozozuz@users.noreply.github.com>
2026-05-13 16:48:38 +0200

    Add src/main/java/dev/unusualfuzzer/UnusualFuzzerExtension.java

---
+++ src/main/java/dev/unusualfuzzer/UnusualFuzzerExtension.java
@@ -0,0 +1,20 @@
+package dev.unusualfuzzer;
+
+import burp.api.montoya.BurpExtension;
+import burp.api.montoya.MontoyaApi;
+
+public final class UnusualFuzzerExtension implements BurpExtension {
+    private static final String EXTENSION_NAME = "UnusualFuzzer";
+
+    @Override
+    public void initialize(MontoyaApi api) {
+        api.extension().setName(EXTENSION_NAME);
+
+        UnusualFuzzerTab tab = new UnusualFuzzerTab(api);
+        api.userInterface().applyThemeToComponent(tab);
+        api.userInterface().registerSuiteTab(EXTENSION_NAME, tab);
+        api.userInterface().registerContextMenuItemsProvider(tab);
+
+        api.logging().logToOutput(EXTENSION_NAME + " loaded");
+    }
+}
